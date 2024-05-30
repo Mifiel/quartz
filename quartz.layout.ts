67037c49f829f2219366dcd129806a76bfdb2e18
Giovanny González Baltazar <giovanny@mifiel.com>
2024-05-30 08:47:50 -0600

    small layout adjustments to see the Explorer all the time

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,10 +16,12 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.Explorer()),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.DesktopOnly(Component.TableOfContents()),
   ],
   left: [
     Component.PageTitle(),
@@ -28,11 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [Component.Graph(), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
